4d1955e696cae2c74b0c63c5b3924c58adac63b7
Gustavo Arranhado <gustavo.arranhado@devoteam.com>
2023-11-07 12:42:02 +0000

    add standalone scroll to view

--- Sources/SupportKitUI/Modifiers/ScrollToViewModifier.swift
+++ Sources/SupportKitUI/Modifiers/ScrollToViewModifier.swift
@@ -37,7 +37,15 @@ extension View {
         self.modifier(ScrollToViewModifier())
     }
     
+    public func scrollToView() -> some View {
+        Color.clear.frame(height: 0).modifier(ScrollToViewModifier())
+    }
+    
     public func scrollToOnChange<Value: Equatable>(_ value: Value) -> some View {
         self.modifier(ScrollToOnChangeViewModifier(value: value))
+    }
+    
+    public func scrollToViewOnChange<Value: Equatable>(_ value: Value) -> some View {
+        Color.clear.frame(height: 0).modifier(ScrollToOnChangeViewModifier(value: value))
     }
 }
