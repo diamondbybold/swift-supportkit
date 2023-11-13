23490e1245377fa9cb562cf7de4ebdff5cadc675
Gustavo Arranhado <gustavo.arranhado@devoteam.com>
2023-11-13 18:03:34 +0000

    add ButtonRole to NavigationButton initializers

--- Sources/SupportKitUI/Navigation/NavigationButton.swift
+++ Sources/SupportKitUI/Navigation/NavigationButton.swift
@@ -2,45 +2,54 @@ import SwiftUI
 
 public struct NavigationButton<Label>: View where Label: View {
     private let label: Label
+    private let role: ButtonRole?
     private let destination: NavigationContext.Destination
     private let content: () -> any View
     
     @EnvironmentObject private var navigationContext: NavigationContext
     
     public init(_ titleKey: LocalizedStringKey,
+                role: ButtonRole? = nil,
                 destination: NavigationContext.Destination,
                 @ViewBuilder content: @escaping () -> any View) where Label == Text {
         self.label = Text(titleKey)
+        self.role = role
         self.destination = destination
         self.content = content
     }
     
-    public init(destination: NavigationContext.Destination,
+    public init(role: ButtonRole? = nil,
+                destination: NavigationContext.Destination,
                 @ViewBuilder content: @escaping () -> any View,
                 @ViewBuilder label: () -> Label) {
         self.label = label()
+        self.role = role
         self.destination = destination
         self.content = content
     }
     
     public init(image: String,
+                role: ButtonRole? = nil,
                 destination: NavigationContext.Destination,
                 @ViewBuilder content: @escaping () -> any View) where Label == Image {
         self.label = Image(image)
+        self.role = role
         self.destination = destination
         self.content = content
     }
     
     public init(systemImage: String,
+                role: ButtonRole? = nil,
                 destination: NavigationContext.Destination,
                 @ViewBuilder content: @escaping () -> any View) where Label == Image {
         self.label = Image(systemName: systemImage)
+        self.role = role
         self.destination = destination
         self.content = content
     }
     
     public var body: some View {
-        Button {
+        Button(role: role) {
             navigationContext.destination(destination, content: content)
         } label: {
             label
