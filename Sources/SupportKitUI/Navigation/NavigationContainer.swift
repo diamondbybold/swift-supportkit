09e6fa257783fdda47abce228d6b2b66d25334a3
Gustavo Arranhado <gustavo.arranhado@devoteam.com>
2023-11-30 17:03:21 +0000

    CustomPathNavigationContainer

--- Sources/SupportKitUI/Navigation/NavigationContainer.swift
+++ Sources/SupportKitUI/Navigation/NavigationContainer.swift
@@ -54,9 +54,66 @@ struct NavigationContainer<Root: View>: View {
     }
 }
 
+struct CustomPathNavigationContainer<Root: View>: View {
+    @StateObject private var navigationContext = NavigationContext()
+    
+    @Environment(\.dismiss) private var dismiss
+    
+    let path: Binding<NavigationPath>
+    
+    @ViewBuilder
+    let root: () -> Root
+    
+    var body: some View {
+        NavigationStack(path: path) {
+            root()
+        }
+        .environmentObject(navigationContext)
+        .sheet(item: $navigationContext.sheet) { item in
+            AnyView(item.content())
+        }
+        .fullScreenCover(item: $navigationContext.fullScreenCover) { item in
+            AnyView(item.content())
+        }
+        .popover(item: $navigationContext.popover) { item in
+            AnyView(item.content())
+        }
+        .confirmationDialog(navigationContext.confirmation?.title ?? "",
+                            isPresented: .present(value: $navigationContext.confirmation),
+                            titleVisibility: .visible) {
+            if let actions = navigationContext.confirmation?.actions {
+                AnyView(actions().environmentObject(navigationContext))
+            }
+        } message: {
+            if let message = navigationContext.confirmation?.message {
+                Text(message)
+            }
+        }
+        .alert(navigationContext.alert?.title ?? "",
+               isPresented: .present(value: $navigationContext.alert)) {
+            if let actions = navigationContext.alert?.actions {
+                AnyView(actions().environmentObject(navigationContext))
+            }
+        } message: {
+            if let message = navigationContext.alert?.message {
+                Text(message)
+            }
+        }
+        .onAppear {
+            navigationContext.onDismiss = {
+                dismiss()
+            }
+        }
+    }
+}
+
 extension View {
     public func navigationContainer() -> some View {
         NavigationContainer { self }
+    }
+    
+    public func navigationContainer(path: Binding<NavigationPath>) -> some View {
+        CustomPathNavigationContainer(path: path) { self }
     }
     
     public func navigationContainer(title: LocalizedStringKey,
