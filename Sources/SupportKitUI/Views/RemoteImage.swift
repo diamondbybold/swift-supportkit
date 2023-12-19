2056c56c1598e5a37ef1555920799cbf1d7fc90a
Gustavo Arranhado <gustavo.arranhado@devoteam.com>
2023-12-19 16:26:55 +0000

    add remote image transition

--- Sources/SupportKitUI/Views/RemoteImage.swift
+++ Sources/SupportKitUI/Views/RemoteImage.swift
@@ -3,31 +3,55 @@ import SwiftUI
 public struct RemoteImage<Placeholder: View>: View {
     private let url: URL?
     private let scaledToFill: Bool
+    private let transition: Bool
     private let placeholder: () -> Placeholder
     
     public init(_ url: URL?,
                 scaledToFill: Bool = false,
+                transition: Bool = true,
                 placeholder: @escaping () -> Placeholder = { EmptyView() }) {
         self.url = url
         self.scaledToFill = scaledToFill
+        self.transition = transition
         self.placeholder = placeholder
     }
     
     public var body: some View {
-        AsyncImage(url: url) { image in
-            if scaledToFill {
-                Color.clear.overlay {
+        if transition {
+            AsyncImage(url: url,
+                       transaction: Transaction(animation: .default)) { phase in
+                if let image = phase.image {
+                    if scaledToFill {
+                        Color.clear.overlay {
+                            image
+                                .resizable()
+                                .scaledToFill()
+                        }
+                    } else {
+                        image
+                            .resizable()
+                            .scaledToFit()
+                    }
+                } else {
+                    placeholder()
+                }
+            }
+        } else {
+            AsyncImage(url: url) { image in
+                if scaledToFill {
+                    Color.clear.overlay {
+                        image
+                            .resizable()
+                            .scaledToFill()
+                    }
+                } else {
                     image
                         .resizable()
-                        .scaledToFill()
+                        .scaledToFit()
                 }
-            } else {
-                image
-                    .resizable()
-                    .scaledToFit()
+            } placeholder: {
+                placeholder()
             }
-        } placeholder: {
-            placeholder()
         }
     }
 }
