900d1b0db8fcb5628dbb9f62460644b7d9b3955b
Gustavo Arranhado <gustavo.arranhado@devoteam.com>
2024-01-22 16:13:22 +0000

    rollback alwaysShowFetchIndicator, will break refresh

--- Sources/SupportKitUI/Views/AsyncView.swift
+++ Sources/SupportKitUI/Views/AsyncView.swift
@@ -3,20 +3,17 @@ import SupportKit
 
 public struct AsyncView<T: Fetchable, Content: View>: View {
     private let fetchable: T
-    private let alwaysShowFetchIndicator: Bool
     private let content: (AsyncViewPhase) -> Content
     
     public init(_ fetchable: T,
-                alwaysShowFetchIndicator: Bool = false,
                 @ViewBuilder content: @escaping (AsyncViewPhase) -> Content) {
         self.fetchable = fetchable
-        self.alwaysShowFetchIndicator = alwaysShowFetchIndicator
         self.content = content
     }
     
     public var body: some View {
         ZStack {
-            if fetchable.isFetching && (alwaysShowFetchIndicator || fetchable.fetchedAt == .distantPast) {
+            if fetchable.isFetching, fetchable.fetchedAt == .distantPast {
                 content(.loading)
             } else if let error = fetchable.error {
                 content(.error(error))
