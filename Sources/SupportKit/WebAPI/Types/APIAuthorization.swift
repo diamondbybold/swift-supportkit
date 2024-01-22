25c69aad6e2e1d57af5c25c93b42cfc8eaa85606
Gustavo Arranhado <gustavo.arranhado@devoteam.com>
2024-01-22 15:29:31 +0000

    fix

--- Sources/SupportKit/WebAPI/Types/APIAuthorization.swift
+++ Sources/SupportKit/WebAPI/Types/APIAuthorization.swift
@@ -4,15 +4,15 @@ public struct APIAuthorization: Codable, Updatable {
     public let accessToken: String
     public let refreshToken: String
     
-    public static var saved: Self? {
-        FileManager.default.cachedItem(name: "authorization", secure: true)
-    }
-    
     public func save() {
         FileManager.default.cacheItem(self, name: "authorization", secure: true)
     }
     
-    public func delete() {
+    public static var saved: Self? {
+        FileManager.default.cachedItem(name: "authorization", secure: true)
+    }
+    
+    public static func delete() {
         FileManager.default.removeCachedItem(name: "authorization", secure: true)
     }
 }
