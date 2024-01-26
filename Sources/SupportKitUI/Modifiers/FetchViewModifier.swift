f6b0201f2b179de84544c2066a89d0238ad1030a
Gustavo Arranhado <gustavo.arranhado@devoteam.com>
2024-01-26 19:59:20 +0000

    rollback fetch modifier improvements

--- Sources/SupportKitUI/Modifiers/FetchViewModifier.swift
+++ Sources/SupportKitUI/Modifiers/FetchViewModifier.swift
@@ -39,53 +39,6 @@ struct FetchViewModifier<T: Fetchable>: ViewModifier {
     }
 }
 
-struct AnyFetchViewModifier: ViewModifier {
-    @Binding var invalidatedAt: Date
-    let expiration: TimeInterval
-    let refreshable: Bool
-    let isActive: Bool
-    let task: () async -> Void
-    
-    @Environment(\.scenePhase) private var phase
-    
-    @State private var fetchedAt: Date = .distantPast
-    
-    private struct FetchTaskId: Equatable {
-        let phase: ScenePhase
-        let invalidateAt: Date
-    }
-    
-    private var isPreview: Bool { ProcessInfo.processInfo.environment["XCODE_RUNNING_FOR_PREVIEWS"] == "1" }
-    
-    private func needsUpdate(_ expiration: TimeInterval = 900) -> Bool {
-        if invalidatedAt > fetchedAt { return true }
-        else { return fetchedAt.hasExpired(in: expiration) }
-    }
-    
-    func body(content: Content) -> some View {
-        Group {
-            if refreshable {
-                content
-                    .refreshable {
-                        if #available(iOS 17, *) {
-                            await task()
-                        } else {
-                            try? await Task.sleep(for: .seconds(0.5))
-                            Task { await task() }
-                        }
-                    }
-            } else {
-                content
-            }
-        }
-        .task(id: FetchTaskId(phase: phase, invalidateAt: invalidatedAt)) {
-            if isActive, (phase == .active || isPreview), needsUpdate(expiration) {
-                await task()
-            }
-        }
-    }
-}
-
 extension View {
     @MainActor
     public func fetch<T: Fetchable>(_ fetchable: T,
@@ -110,19 +63,6 @@ extension View {
                                         refreshable: refreshable,
                                         isActive: isActive,
                                         task: task))
-    }
-    
-    @MainActor
-    public func fetch(invalidatedAt: Binding<Date> = .constant(.distantPast),
-                      expiration: TimeInterval = 900,
-                      refreshable: Bool = false,
-                      isActive: Bool = true,
-                      task: @escaping () async -> Void) -> some View {
-        self.modifier(AnyFetchViewModifier(invalidatedAt: invalidatedAt,
-                                           expiration: expiration,
-                                           refreshable: refreshable,
-                                           isActive: isActive,
-                                           task: task))
     }
     
     @MainActor
