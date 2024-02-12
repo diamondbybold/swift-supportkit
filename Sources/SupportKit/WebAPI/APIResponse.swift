329db9f4dd78c1be9b9d94a32dea5bda2cbb3b4a
Gustavo Arranhado <gustavo.arranhado@devoteam.com>
2024-02-12 16:41:33 +0000

    improvement

--- Sources/SupportKit/WebAPI/APIResponse.swift
+++ Sources/SupportKit/WebAPI/APIResponse.swift
@@ -187,6 +187,7 @@ extension APIResponse {
 
 // MARK: - Utils
 extension Array: Error, LocalizedError where Element == APIResponse.ContainerError {
+    public var errorCode: String? { first?.code }
     public var failureReason: String? { first?.failureReason }
     public var recoverySuggestion: String? { first?.recoverySuggestion }
 }
