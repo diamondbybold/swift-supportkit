bc2037ff8d218c65d5c1c9410fcfebde0d5a94a6
Gustavo Arranhado <gustavo.arranhado@devoteam.com>
2024-03-04 16:45:20 +0000

    add success message option to AsyncButton

--- Sources/SupportKitUI/Views/AsyncButton.swift
+++ Sources/SupportKitUI/Views/AsyncButton.swift
@@ -2,9 +2,12 @@ import SwiftUI
 import SupportKit
 
 public struct AsyncButton<Label>: View where Label: View {
+    public typealias SuccessMessage = (title: LocalizedStringKey, description: LocalizedStringKey)
+    
     private let label: Label
     private let role: ButtonRole?
     private let debounce: Bool
+    private let successMessage: SuccessMessage?
     private let action: () async throws -> Void
     
     @State private var waiting: Bool = false
@@ -13,40 +16,48 @@ public struct AsyncButton<Label>: View where Label: View {
     
     public init(_ titleKey: LocalizedStringKey,
                 role: ButtonRole? = nil,
+                successMessage: SuccessMessage? = nil,
                 debounce: Bool = true,
                 action: @escaping () async throws -> Void) where Label == Text {
         self.label = Text(titleKey)
         self.role = role
+        self.successMessage = successMessage
         self.debounce = debounce
         self.action = action
     }
     
     public init(role: ButtonRole? = nil,
+                successMessage: SuccessMessage? = nil,
                 action: @escaping () async throws -> Void,
                 debounce: Bool = true,
                 @ViewBuilder label: () -> Label) {
         self.label = label()
         self.role = role
+        self.successMessage = successMessage
         self.debounce = debounce
         self.action = action
     }
     
     public init(image: String,
                 role: ButtonRole? = nil,
+                successMessage: SuccessMessage? = nil,
                 debounce: Bool = true,
                 action: @escaping () async throws -> Void) where Label == Image {
         self.label = Image(image)
         self.role = role
+        self.successMessage = successMessage
         self.debounce = debounce
         self.action = action
     }
     
     public init(systemImage: String,
                 role: ButtonRole? = nil,
+                successMessage: SuccessMessage? = nil,
                 debounce: Bool = true,
                 action: @escaping () async throws -> Void) where Label == Image {
         self.label = Image(systemName: systemImage)
         self.role = role
+        self.successMessage = successMessage
         self.debounce = debounce
         self.action = action
     }
@@ -56,6 +67,14 @@ public struct AsyncButton<Label>: View where Label: View {
         
         do {
             try await action()
+            
+            if let successMessage {
+                navigationContext.alert(title: successMessage.title,
+                                        message: successMessage.description,
+                                        confirmation: false) {
+                    Button("OK") { }
+                }
+            }
         } catch let e as LocalizedError {
             navigationContext.alert(title: LocalizedStringKey(e.failureReason ?? ""),
                                     message: LocalizedStringKey(e.recoverySuggestion ?? ""),
