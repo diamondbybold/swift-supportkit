384faaa0df094e57f6161c5410f8d8c4f9c123fa
Gustavo Arranhado <gustavo.arranhado@devoteam.com>
2024-03-25 16:42:18 +0000

    alertError view modifier

---
+++ Sources/SupportKitUI/Modifiers/AlertErrorViewModifier.swift
@@ -0,0 +1,21 @@
+import SwiftUI
+
+struct AlertErrorViewModifier: ViewModifier {
+    @Binding var error: Error?
+    
+    var localizedError: LocalizedError? { error as? LocalizedError }
+    
+    func body(content: Content) -> some View {
+        content.alert(localizedError?.failureReason ?? "", isPresented: .present(value: $error)) {
+            Button("OK", role: .cancel) { }
+        } message: {
+            Text(localizedError?.recoverySuggestion ?? localizedError?.localizedDescription ?? "")
+        }
+    }
+}
+
+extension View {
+    public func alertError(_ error: Binding<Error?>) -> some View {
+        self.modifier(AlertErrorViewModifier(error: error))
+    }
+}
