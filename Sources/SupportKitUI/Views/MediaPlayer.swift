12c3fd9f57f6cc46cab5775b834775037867fcda
Gustavo Arranhado <gustavo.arranhado@devoteam.com>
2024-03-26 16:45:43 +0000

    change media player object access

--- Sources/SupportKitUI/Views/MediaPlayer.swift
+++ Sources/SupportKitUI/Views/MediaPlayer.swift
@@ -1,13 +1,23 @@
 import AVKit
 import SwiftUI
 
-struct MediaPlayer: UIViewControllerRepresentable {
-    let player: AVPlayer
-    var showsPlaybackControls: Bool = true
-    var allowsPictureInPicturePlayback: Bool = true
-    var videoGravity: AVLayerVideoGravity = .resizeAspect
+public struct MediaPlayer: UIViewControllerRepresentable {
+    let player: AVPlayer?
+    var showsPlaybackControls: Bool
+    var allowsPictureInPicturePlayback: Bool
+    var videoGravity: AVLayerVideoGravity
     
-    func makeUIViewController(context: Context) -> AVPlayerViewController {
+    init(player: AVPlayer?,
+         showsPlaybackControls: Bool = true,
+         allowsPictureInPicturePlayback: Bool = true,
+         videoGravity: AVLayerVideoGravity = .resizeAspect) {
+        self.player = player
+        self.showsPlaybackControls = showsPlaybackControls
+        self.allowsPictureInPicturePlayback = allowsPictureInPicturePlayback
+        self.videoGravity = videoGravity
+    }
+    
+    public func makeUIViewController(context: Context) -> AVPlayerViewController {
         let controller = AVPlayerViewController()
         controller.modalPresentationStyle = .fullScreen
         controller.player = player
@@ -17,7 +27,7 @@ struct MediaPlayer: UIViewControllerRepresentable {
         return controller
     }
     
-    func updateUIViewController(_ playerController: AVPlayerViewController, context: Context) {
+    public func updateUIViewController(_ playerController: AVPlayerViewController, context: Context) {
         playerController.player = player
         playerController.showsPlaybackControls = showsPlaybackControls
         playerController.allowsPictureInPicturePlayback = allowsPictureInPicturePlayback
