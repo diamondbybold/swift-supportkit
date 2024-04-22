337ba3fa33783bea7e2af14fd4912bbd40453552
Gustavo Arranhado <gustavo.arranhado@devoteam.com>
2024-04-22 20:59:14 +0100

    -

--- Sources/SupportKit/AnalyticsProtocol.swift
+++ Sources/SupportKit/AnalyticsProtocol.swift
@@ -3,9 +3,9 @@ import Foundation
 public var sharedAnalyticsObject: (any AnalyticsProtocol)? = nil
 
 public protocol AnalyticsProtocol: AnyObject {
-    var userIdentifier: String { get set }
+    var userIdentifier: String? { get set }
     
-    func logEvent(_ event: AnalyticsEvent, name: String, identifier: String, parameters: [String: Any?]?)
+    func logEvent(_ event: AnalyticsEvent, name: String, identifier: String?, parameters: [String: Any?]?)
 }
 
 public enum AnalyticsEvent {
