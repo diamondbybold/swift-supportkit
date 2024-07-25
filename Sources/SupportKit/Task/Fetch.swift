1de4d3e51796de5f60e296d6dbdc2fbb04707027
Gustavo Arranhado <gustavo.arranhado@devoteam.com>
2024-07-25 18:08:56 +0100

    review

--- Sources/SupportKit/Task/Fetch.swift
+++ Sources/SupportKit/Task/Fetch.swift
@@ -53,78 +53,80 @@ extension FetchState {
     }
 }
 
-private func prepareForFetch(_ state: inout FetchState,
-                             refreshing: Bool? = nil) {
-    if let refreshing {
-        if !refreshing { state = .waiting }
-    } else {
-        if state.error != nil || state.isEmpty || state.page > 1 { state = .waiting }
+extension Task where Success == Never, Failure == Never {
+    private static func prepareForFetch(_ state: inout FetchState,
+                                        refreshing: Bool? = nil) {
+        if let refreshing {
+            if !refreshing { state = .waiting }
+        } else {
+            if state.error != nil || state.isEmpty || state.page > 1 { state = .waiting }
+        }
     }
-}
-
-public func fetch<T>(_ state: inout FetchState,
-                     in store: inout T?,
-                     refreshing: Bool? = nil,
-                     task: () async throws -> T?) async {
-    prepareForFetch(&state, refreshing: refreshing)
     
-    do {
-        store = try await task()
-        state = .success(.init(page: 1, total: 1, partial: false))
-    } catch is CancellationError {
-    } catch {
-        state = .failure(error)
-    }
-}
-
-public func fetch<T>(_ state: inout FetchState,
-                     in store: inout [T],
-                     refreshing: Bool? = nil,
-                     task: () async throws -> [T]) async {
-    prepareForFetch(&state, refreshing: refreshing)
-    
-    do {
-        store = try await task()
-        state = .success(.init(page: 1, total: store.count, partial: false))
-    } catch is CancellationError {
-    } catch {
-        state = .failure(error)
-    }
-}
-
-public func fetch<T>(_ state: inout FetchState,
-                     in store: inout [T],
-                     refreshing: Bool? = nil,
-                     task: (Int) async throws -> ([T], Int)) async {
-    prepareForFetch(&state, refreshing: refreshing)
-    
-    do {
-        let result = try await task(1)
-        let count = result.0.count
-        let total = result.1
+    public static func fetch<T>(_ state: inout FetchState,
+                                in store: inout T?,
+                                refreshing: Bool? = nil,
+                                task: () async throws -> T?) async {
+        prepareForFetch(&state, refreshing: refreshing)
         
-        store = result.0
-        state = .success(.init(page: 1, total: total, partial: count < total))
-    } catch is CancellationError {
-    } catch {
-        state = .failure(error)
+        do {
+            store = try await task()
+            state = .success(.init(page: 1, total: 1, partial: false))
+        } catch is CancellationError {
+        } catch {
+            state = .failure(error)
+        }
     }
-}
-
-public func fetchMore<T>(_ state: inout FetchState,
-                         in store: inout [T],
-                         task: (Int) async throws -> ([T], Int)) async {
-    let nextPage = state.page + 1
     
-    do {
-        let result = try await task(nextPage)
-        let count = result.0.count
-        let total = result.1
+    public static func fetch<T>(_ state: inout FetchState,
+                                in store: inout [T],
+                                refreshing: Bool? = nil,
+                                task: () async throws -> [T]) async {
+        prepareForFetch(&state, refreshing: refreshing)
         
-        store.append(contentsOf: result.0)
-        state = .success(.init(page: nextPage, total: total, partial: count < total))
-    } catch is CancellationError {
-    } catch {
-        state = .failure(error)
+        do {
+            store = try await task()
+            state = .success(.init(page: 1, total: store.count, partial: false))
+        } catch is CancellationError {
+        } catch {
+            state = .failure(error)
+        }
+    }
+    
+    public static func fetch<T>(_ state: inout FetchState,
+                                in store: inout [T],
+                                refreshing: Bool? = nil,
+                                task: (Int) async throws -> ([T], Int)) async {
+        prepareForFetch(&state, refreshing: refreshing)
+        
+        do {
+            let result = try await task(1)
+            let count = result.0.count
+            let total = result.1
+            
+            store = result.0
+            state = .success(.init(page: 1, total: total, partial: count < total))
+        } catch is CancellationError {
+        } catch {
+            state = .failure(error)
+        }
+    }
+    
+    public static func fetchMore<T>(_ state: inout FetchState,
+                                    in store: inout [T],
+                                    task: (Int) async throws -> ([T], Int)) async {
+        let nextPage = state.page + 1
+        
+        do {
+            let result = try await task(nextPage)
+            let count = result.0.count
+            let total = result.1
+            
+            store.append(contentsOf: result.0)
+            state = .success(.init(page: nextPage, total: total, partial: count < total))
+        } catch is CancellationError {
+        } catch {
+            state = .failure(error)
+        }
     }
 }
